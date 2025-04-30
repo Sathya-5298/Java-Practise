c6fd716ff2d892ad6838ec34d82f50608c45f9e2
smurt <smurt@host.docker.internal>
2025-04-30 10:26:45 +0530

    longest

--- src/com/qa/java/parctise/LongestSubstringWithoutRepeatingCharacters.java
+++ src/com/qa/java/parctise/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,14 +1,37 @@
 package com.qa.java.parctise;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class LongestSubstringWithoutRepeatingCharacters 
 {
 	public static void main(String[] args) 
 	{
 		String input = "abcdbbcda";
-		System.out.println(longestSubString(input));
+	//	System.out.println(longestSubString(input));
+		System.out.println(findLongestSubstring(input));
 	}
+
+	    public static int findLongestSubstring(String s) 
+	    {
+	        HashSet<Character> set = new HashSet<Character>();
+	        int maxLength = 0;
+	        int left = 0;
+
+	        for (int right = 0; right < s.length(); right++) {
+	            // Remove characters from the set if there’s a duplicate
+	            while (set.contains(s.charAt(right))) {
+	                set.remove(s.charAt(left));
+	                left++; // Move the left pointer
+	            }
+	            // Add the current character to the set
+	            set.add(s.charAt(right));
+	            // Update the maximum length
+	            maxLength = Math.max(maxLength, right - left + 1);
+	        }
+	        return maxLength;
+	    }
+
 	
 	public static int longestSubString(String str)
 	{
@@ -28,5 +51,7 @@ public class LongestSubstringWithoutRepeatingCharacters
 		}
 		System.out.println(map.keySet());
 		return maxLength;
+		
+		
 	}
 }
