1b17fabee7b0ec5d4430afc5c03c134a31a6f43c
smurt <smurt@192.168.1.65>
2025-06-27 14:56:41 +0530

    an

---
+++ src/com/qa/practise/stringsqs/StringAnagarams.java
@@ -0,0 +1,23 @@
+package com.qa.practise.stringsqs;
+
+import java.util.Arrays;
+
+public class StringAnagarams {
+
+	public static void main(String[] args) 
+	{
+		String s1 = "listen";
+		String s2 = "silent";
+		
+		char[] ch1 = s1.toLowerCase().toCharArray();
+		char[] ch2 = s2.toLowerCase().toCharArray();
+		
+		Arrays.sort(ch1);
+		Arrays.sort(ch2);
+		
+		boolean b = Arrays.equals(ch1, ch2);
+		System.out.println(b);
+		
+		
+	}
+}
