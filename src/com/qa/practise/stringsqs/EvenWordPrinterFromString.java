aa0eab28349323a65f50df3daf01cd5278ed8e1a
smurt <smurt@192.168.1.40>
2025-07-13 12:19:46 +0530

    poj

--- src/com/qa/practise/stringsqs/EvenWordPrinterFromString.java
+++ src/com/qa/practise/stringsqs/EvenWordPrinterFromString.java
@@ -1,22 +1,18 @@
 package com.qa.practise.stringsqs;
 
-import java.util.StringTokenizer;
-
 public class EvenWordPrinterFromString {
 
 	public static void main(String[] args) 
 	{
-		String s = "TODO Auto-generated method stub";
-		
-		StringTokenizer tokenizer = new StringTokenizer(s);
-		
-		while(tokenizer.hasMoreTokens())
-		{
-			String eachToken = tokenizer.nextToken();
-			if(eachToken.length() % 2 == 0)
-			{
-				System.out.println(eachToken);
-			}
-		}
+		 String s = "Sky is blue and vast";
+		    String[] words = s.split(" ");
+		    
+		    for(String word: words)
+		    {
+		        if(word.length() % 2 == 0)
+		        {
+		            System.out.println(word);
+		        }
+		    }
 	}
 }
