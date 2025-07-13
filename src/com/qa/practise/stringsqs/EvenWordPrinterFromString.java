8ebf08f9241a94f486222c29a79d3be8676b31c4
smurt <smurt@192.168.1.40>
2025-07-13 12:20:14 +0530

    code changes

--- src/com/qa/practise/stringsqs/EvenWordPrinterFromString.java
+++ src/com/qa/practise/stringsqs/EvenWordPrinterFromString.java
@@ -5,7 +5,7 @@ public class EvenWordPrinterFromString {
 	public static void main(String[] args) 
 	{
 		 String s = "Sky is blue and vast";
-		    String[] words = s.split(" ");
+		 String[] words = s.split(" ");
 		    
 		    for(String word: words)
 		    {
