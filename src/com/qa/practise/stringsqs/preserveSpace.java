6e3582689490ef1c11b82c01188fc4203c456548
sdevalap <sdevalap@LIN-CR2C2J3.corp.capgemini.com>
2025-07-24 12:23:20 -0500

    kjh

---
+++ src/com/qa/practise/stringsqs/preserveSpace.java
@@ -0,0 +1,39 @@
+package com.qa.practise.stringsqs;
+
+public class preserveSpace {
+
+	public static void main(String[] args) 
+	{
+		String str = "AWS Cloud Quest: Recertify Cloud Practitioner";
+		
+		int n = str.length();
+		int start = 0;
+		int end = n - 1;
+		
+		char[] ch = str.toCharArray();
+		
+		while(start < end)
+		{
+			if(ch[start] == ' ')
+			{
+				start++;
+				continue;
+			}
+			else if(ch[end] == ' ')
+			{
+				end--;
+				continue;
+			}
+			else
+			{
+				char temp = ch[start];
+				ch[start] = ch[end];
+				ch[end] = temp;
+				
+				start++;
+				end--;
+			}
+		}
+		System.out.println(ch);
+	}
+}
