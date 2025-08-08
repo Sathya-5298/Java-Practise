5eca058759c5d3dfedbf8c8d8831eec82efc2988
smurt <smurt@192.168.1.24>
2025-08-08 11:20:22 +0530

    d

--- src/com/qa/practise/stringsqs/ExpandCharactersDigit.java
+++ src/com/qa/practise/stringsqs/ExpandCharactersDigit.java
@@ -7,20 +7,19 @@ public class ExpandCharactersDigit {
 		String input = "a1b2c3d4";
 		
 		for(int i = 0; i < input.length(); i++)
-		{
-			if(Character.isAlphabetic(input.charAt(i)))
-			{
-				System.out.print(input.charAt(i));
-			}
-			else
-			{
-				int x = Character.getNumericValue(input.charAt(i));
-				
-				for(int j = 1; j < x; j++)
-				{
-					System.out.print(input.charAt(i - 1));
-				}
-			}
-		}
+	    {
+	        if(Character.isAlphabetic(input.charAt(i)))
+	        {
+	            System.out.print(input.charAt(i));
+	        }
+	        else
+	        {
+	            int x = Character.getNumericValue(input.charAt(i));
+	            for(int j = 1; j < x; j++)
+	            {
+	                System.out.print(input.charAt(i - 1));
+	            }
+	        }
+	    }
 	}
 }
