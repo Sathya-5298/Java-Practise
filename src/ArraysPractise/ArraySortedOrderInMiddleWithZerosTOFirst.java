ee729d2f4ce0dc11caaa50ece443804b35644bae
smurt <smurt@LAPTOP-88JOOBUG>
2025-09-05 12:24:47 +0530

    zsxdf

---
+++ src/ArraysPractise/ArraySortedOrderInMiddleWithZerosTOFirst.java
@@ -0,0 +1,44 @@
+package ArraysPractise;
+
+import java.util.Arrays;
+
+public class ArraySortedOrderInMiddleWithZerosTOFirst {
+
+	public static void main(String[] args) 
+	{
+		// Input [1,3,1,0,2,1,4,6,8,7,9,0,0,0,9]
+		// Output [0,0,0,0,2,3,4,5,6,7,8,9,9,1,1,1]
+		
+		int[] arr = {1,3,1,0,2,1,4,6,8,7,9,0,0,0,9};
+		
+		Arrays.sort(arr);
+		
+		int[] result  = new int[arr.length];
+		
+		int left = 0;
+		int right = arr.length - 1;
+		
+		for(int num : arr)
+		{
+			if(num == 0)
+			{
+				result[left++] = num;
+			}
+			else if(num == 1)
+			{
+				result[right--] = num;
+			}
+			else
+			{
+				result[left++] = num;
+			}
+		}
+		
+		for(int num : result)
+		{
+			System.out.println(num);
+		}
+		
+		
+	}
+}
